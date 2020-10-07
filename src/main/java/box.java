579b48719eacb65c6a7e60d4babcd0b917f1c86c
diodes12581 <2265984071@qq.com>
2020-10-07 22:40:52 +0800

    ChuqiWu

--- src/main/java/box.java
+++ src/main/java/box.java
@@ -1,12 +1,16 @@
 import com.lowagie.text.DocumentException;
 
 import javax.swing.*;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+import javax.swing.text.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.text.DateFormat;
+import java.util.HashSet;
+import java.util.Set;
 
 public class box extends JFrame implements ActionListener {
 
@@ -20,7 +24,7 @@ public class box extends JFrame implements ActionListener {
     //对于edit菜单的子项
     private JMenuItem item_about;
     //对于help菜单的子项
-    private static JTextArea edit_text_area;
+    private static JTextPane edit_text_pane;
     //编辑区域
     private JScrollPane scroll_bar;
     //可滚动的pane 里面添加edit_text_area就可以变为一个可以滚动的文本框，JScrollPane是一个pane，同时可以设置方向
@@ -100,8 +104,9 @@ public class box extends JFrame implements ActionListener {
      * 同时对pane设置方向
      */
     public void initEditArea() {
-        edit_text_area = new JTextArea();
-        scroll_bar = new JScrollPane(edit_text_area);
+        edit_text_pane = new JTextPane();
+        edit_text_pane.getDocument().addDocumentListener(new SyntaxHighlighter(edit_text_pane));
+        scroll_bar = new JScrollPane(edit_text_pane);
         scroll_bar.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
     }
 
@@ -128,27 +133,27 @@ public class box extends JFrame implements ActionListener {
         }else if (e.getSource() == item_exit) {
             this.dispose();
         }else if (e.getSource() == item_open) {
-            fileFunction.open(edit_text_area, scroll_bar);
+            fileFunction.open(edit_text_pane, scroll_bar);
         }else if (e.getSource() == item_save) {
-            fileFunction.save(edit_text_area, scroll_bar);
+            fileFunction.save(edit_text_pane, scroll_bar);
         }else if (e.getSource() == item_print) {
             fileFunction.print();
         }else if (e.getSource() == item_export) {
             try {
-                fileFunction.export(edit_text_area, scroll_bar);
+                fileFunction.export(edit_text_pane, scroll_bar);
             } catch (IOException | DocumentException ex) {
                 ex.printStackTrace();
             }
         } else if (e.getSource() == item_search) {
             search();
         }else if (e.getSource() == item_undo) {
-            editFunction.undo(edit_text_area);
+            editFunction.undo(edit_text_pane);
         }else if (e.getSource() == item_cut) {
-            editFunction.cut(edit_text_area);
+            editFunction.cut(edit_text_pane);
         }else if (e.getSource() == item_copy) {
-            editFunction.copy(edit_text_area);
+            editFunction.copy(edit_text_pane);
         }else if (e.getSource() == item_stick) {
-            editFunction.paste(edit_text_area);
+            editFunction.paste(edit_text_pane);
         }else if (e.getSource() == item_about) {
             new helpFunction.about_Window();
         }
@@ -187,7 +192,7 @@ public class box extends JFrame implements ActionListener {
         {   //"区分大小写(C)"的JCheckBox是否被选中
             int k=0,m=0;
             final String str1,str2,str3,str4,strA,strB;
-            str1=edit_text_area.getText();
+            str1= edit_text_pane.getText();
             str2=findText.getText();
             str3=str1.toUpperCase();
             str4=str2.toUpperCase();
@@ -201,28 +206,28 @@ public class box extends JFrame implements ActionListener {
             }
             if(upButton.isSelected())
             {   //k=strA.lastIndexOf(strB,editArea.getCaretPosition()-1);
-                if(edit_text_area.getSelectedText()==null)
-                    k=strA.lastIndexOf(strB,edit_text_area.getCaretPosition()-1);
+                if(edit_text_pane.getSelectedText()==null)
+                    k=strA.lastIndexOf(strB, edit_text_pane.getCaretPosition()-1);
                 else
-                    k=strA.lastIndexOf(strB, edit_text_area.getCaretPosition()-findText.getText().length()-1);
+                    k=strA.lastIndexOf(strB, edit_text_pane.getCaretPosition()-findText.getText().length()-1);
                 if(k>-1)
                 {   //String strData=strA.subString(k,strB.getText().length()+1);
-                    edit_text_area.setCaretPosition(k);
-                    edit_text_area.select(k,k+strB.length());
+                    edit_text_pane.setCaretPosition(k);
+                    edit_text_pane.select(k,k+strB.length());
                 }
                 else
                 {   JOptionPane.showMessageDialog(null,"找不到您查找的内容！","查找",JOptionPane.INFORMATION_MESSAGE);
                 }
             }
             else if(downButton.isSelected())
-            {   if(edit_text_area.getSelectedText()==null)
-                k=strA.indexOf(strB,edit_text_area.getCaretPosition()+1);
+            {   if(edit_text_pane.getSelectedText()==null)
+                k=strA.indexOf(strB, edit_text_pane.getCaretPosition()+1);
             else
-                k=strA.indexOf(strB, edit_text_area.getCaretPosition()-findText.getText().length()+1);
+                k=strA.indexOf(strB, edit_text_pane.getCaretPosition()-findText.getText().length()+1);
                 if(k>-1)
                 {   //String strData=strA.subString(k,strB.getText().length()+1);
-                    edit_text_area.setCaretPosition(k);
-                    edit_text_area.select(k,k+strB.length());
+                    edit_text_pane.setCaretPosition(k);
+                    edit_text_pane.select(k,k+strB.length());
                 }
                 else
                 {   JOptionPane.showMessageDialog(null,"找不到您查找的内容！","查找",JOptionPane.INFORMATION_MESSAGE);
@@ -256,8 +261,198 @@ public class box extends JFrame implements ActionListener {
         findDialog.setVisible(true);
     }//查找方法结束
 
+    public class SyntaxHighlighter implements DocumentListener {
+        private Set<String> keywords;
+        private Style keywordStyle;
+        private Style normalStyle;
+
+        public SyntaxHighlighter(JTextPane editor) {
+
+            // 准备着色使用的样式
+            keywordStyle = ((StyledDocument) editor.getDocument()).addStyle("Keyword_Style", null);
+            normalStyle = ((StyledDocument) editor.getDocument()).addStyle("Keyword_Style", null);
+            StyleConstants.setForeground(keywordStyle, Color.orange);
+            StyleConstants.setForeground(normalStyle, Color.BLACK);
+
+
+            // 准备关键字
+            keywords = new HashSet<String>();
+            keywords.add("public");
+            keywords.add("class");
+            keywords.add("protected");
+            keywords.add("private");
+            keywords.add("_int_");
+            keywords.add("float");
+            keywords.add("double");
+        }
+
+        public void colouring(StyledDocument doc, int pos, int len) throws BadLocationException {
+
+            // 取得插入或者删除后影响到的单词.
+            // 例如"public"在b后插入一个空格, 就变成了:"pub lic", 这时就有两个单词要处理:"pub"和"lic"
+            // 这时要取得的范围是pub中p前面的位置和lic中c后面的位置
+
+            int start = indexOfWordStart(doc, pos);
+            int end = indexOfWordEnd(doc, pos + len);
+
+            char ch;
+            while (start < end) {
+                ch = getCharAt(doc, start);
+                if (Character.isLetter(ch) || ch == '_') {
+                    // 如果是以字母或者下划线开头, 说明是单词
+                    // pos为处理后的最后一个下标
+                    start = colouringWord(doc, start);
+                } else {
+                    SwingUtilities.invokeLater(new ColouringTask(doc, start, 1, normalStyle));
+
+                    ++start;
+                }
+            }
+        }
+
+        /**
+         * 对单词进行着色, 并返回单词结束的下标.
+         * @param doc
+         * @param pos
+         * @return
+         * @throws BadLocationException
+         */
+        public int colouringWord(StyledDocument doc, int pos) throws BadLocationException {
+
+            int wordEnd = indexOfWordEnd(doc, pos);
+            String word = doc.getText(pos, wordEnd - pos);
+
+            if (keywords.contains(word)) {
+
+                // 如果是关键字, 就进行关键字的着色, 否则使用普通的着色.
+                // 这里有一点要注意, 在insertUpdate和removeUpdate的方法调用的过程中, 不能修改doc的属性.
+                // 但我们又要达到能够修改doc的属性, 所以把此任务放到这个方法的外面去执行.
+                // 实现这一目的, 可以使用新线程, 但放到swing的事件队列里去处理更轻便一点.
+
+                SwingUtilities.invokeLater(new ColouringTask(doc, pos, wordEnd - pos, keywordStyle));
+            } else {
+                SwingUtilities.invokeLater(new ColouringTask(doc, pos, wordEnd - pos, normalStyle));
+            }
+            return wordEnd;
+        }
+
+        /**
+         * 取得在文档中下标在pos处的字符.
+         * 如果pos为doc.getLength(), 返回的是一个文档的结束符, 不会抛出异常. 如果pos<0, 则会抛出异常.
+         * 所以pos的有效值是[0, doc.getLength()]
+         * @param doc
+         * @param pos
+         * @return
+         * @throws BadLocationException
+
+         */
+        public char getCharAt(Document doc, int pos) throws BadLocationException {
+
+            return doc.getText(pos, 1).charAt(0);
+        }
+
+        /**
+         * 取得下标为pos时, 它所在的单词开始的下标. Â±wor^dÂ± (^表示pos, Â±表示开始或结束的下标)
+         * @param doc
+         * @param pos
+         * @return
+         * @throws BadLocationException
+         */
+
+        public int indexOfWordStart(Document doc, int pos) throws BadLocationException {
+
+            // 从pos开始向前找到第一个非单词字符.
+            for (; pos > 0 && isWordCharacter(doc, pos - 1); --pos);
+            return pos;
+        }
+
+        /**
+         * 取得下标为pos时, 它所在的单词结束的下标. Â±wor^dÂ± (^表示pos, Â±表示开始或结束的下标)
+         * @param doc
+         * @param pos
+         * @return
+         * @throws BadLocationException
+         */
+
+        public int indexOfWordEnd(Document doc, int pos) throws BadLocationException {
+
+            // 从pos开始向前找到第一个非单词字符.
+
+            for (; isWordCharacter(doc, pos); ++pos);
+
+            return pos;
+        }
+        /**
+         * 如果一个字符是字母, 数字, 下划线, 则返回true.
+         *
+         * @param doc
+         * @param pos
+         * @return
+         * @throws BadLocationException
+         */
+
+        public boolean isWordCharacter(Document doc, int pos) throws BadLocationException {
+
+            char ch = getCharAt(doc, pos);
+            if (Character.isLetter(ch) || Character.isDigit(ch) || ch == '_') { return true; }
+            return false;
+        }
+
+        @Override
+        public void changedUpdate(DocumentEvent e) {
+
+        }
+
+        @Override
+        public void insertUpdate(DocumentEvent e) {
+
+            try {
+                colouring((StyledDocument) e.getDocument(), e.getOffset(), e.getLength());
+            } catch (BadLocationException e1) {
+                e1.printStackTrace();
+            }
+        }
+
+        @Override
+        public void removeUpdate(DocumentEvent e) {
+            try {
+                // 因为删除后光标紧接着影响的单词两边, 所以长度就不需要了
+                colouring((StyledDocument) e.getDocument(), e.getOffset(), 0);
+            } catch (BadLocationException e1) {
+                e1.printStackTrace();
+            }
+        }
+
+        /**
+         * 完成着色任务
+         * @author Biao
+         */
+        private class ColouringTask implements Runnable {
+            private StyledDocument doc;
+            private Style style;
+            private int pos;
+            private int len;
+
+            public ColouringTask(StyledDocument doc, int pos, int len, Style style) {
+
+                this.doc = doc;
+                this.pos = pos;
+                this.len = len;
+                this.style = style;
+            }
+
+            public void run() {
+                try {
+                    // 这里就是对字符进行着色
+                    doc.setCharacterAttributes(pos, len, style, true);
+                } catch (Exception e) {}
+            }
+        }
+    }
 
     public static void main(String[] args) {
         box b = new box();
     }
+
 }
+
