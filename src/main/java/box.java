626f15c7e9a0aa5b13c240af57f153ef0f5fe8de
diodes12581 <2265984071@qq.com>
2020-10-10 00:28:28 +0800

    ChuqiWu

--- src/main/java/box.java
+++ src/main/java/box.java
@@ -26,22 +26,23 @@ public class box extends JFrame implements ActionListener {
     JSONObject jobj = JSON.parseObject(json);
 
     private JMenuBar menuBar;
-    //右键弹出菜单项
+    //Right-click to bring up a menu item
     JPopupMenu popupMenu;
     JMenuItem popupMenu_Cut,popupMenu_Copy,popupMenu_Paste,popupMenu_SelectAll;
-    //菜单栏
+    //The menu bar
     private JMenu menu_File,menu_Search,menu_Edit,menu_Help;
-    //菜单栏内的菜单
+    //The menu in the menu bar
     private JMenuItem item_new,item_open,item_save,item_exit,item_print,item_export, item_search;
-    //对于file菜单的子项
+    //For subitems of the File menu
     private JMenuItem item_selectAll,item_cut,item_copy,item_stick;
-    //对于edit菜单的子项
+    //For children of the Edit menu
     private JMenuItem item_about;
-    //对于help菜单的子项
+    //For the children of the Help menu
     private static JTextPane edit_text_pane;
-    //编辑区域
+    //Editing area
     private JScrollPane scroll_bar;
-    //可滚动的pane 里面添加edit_text_area就可以变为一个可以滚动的文本框，JScrollPane是一个pane，同时可以设置方向
+    //Add edit_text_area to a scrollable Pane to make it a scrollable text box.
+    // A JScrollPane is an pane and can set the direction
 
     public box() {
         initMenuBar();
@@ -60,13 +61,13 @@ public class box extends JFrame implements ActionListener {
     }
 
     /**
-     * 两种形式：
-     * menu_File.setMnemonic('f'); 对menu
-     * item_word_format.setAccelerator(KeyStroke.getKeyStroke('F',java.awt.Event.CTRL_MASK,false)); 对item
+     * Two shortcuts：
+     * menu_File.setMnemonic('f'); for menu
+     * item_word_format.setAccelerator(KeyStroke.getKeyStroke('F',java.awt.Event.CTRL_MASK,false)); for item
      */
     public void initMenuBar() {
 
-        //创建右键弹出菜单
+        //Create a right-click pop-up menu
         popupMenu=new JPopupMenu();
         popupMenu_Cut=new JMenuItem("Cut(T)");
         popupMenu_Copy=new JMenuItem("Copy(C)");
@@ -74,7 +75,7 @@ public class box extends JFrame implements ActionListener {
         popupMenu_SelectAll=new JMenuItem("SelectAll(A)");
 
 
-        //向右键菜单添加菜单项和分隔符
+        //Adds a menu item and separator to a right-click menu
         popupMenu.add(popupMenu_Cut);
         popupMenu.add(popupMenu_Copy);
         popupMenu.add(popupMenu_Paste);
@@ -82,7 +83,7 @@ public class box extends JFrame implements ActionListener {
 
         menuBar = new JMenuBar();
         menu_File = new JMenu("File");
-        menu_File.setMnemonic('f');//f+alt打开
+        menu_File.setMnemonic('f');//f+alt to open
         item_new = new JMenuItem("New");
         item_open = new JMenuItem("Open");
         item_save = new JMenuItem("Save");
@@ -96,13 +97,13 @@ public class box extends JFrame implements ActionListener {
         menu_File.add(item_export);
         menu_File.add(item_exit);
 
-        //File 菜单
+        //The File menu
 
         menu_Search = new JMenu("Search");
         menu_Search.setMnemonic('s');
         item_search = new JMenuItem("Search");
         menu_Search.add(item_search);
-        //Search 菜单
+        //The Search menu
 
         menu_Edit = new JMenu("Edit");
         menu_Edit.setMnemonic('e');
@@ -114,13 +115,13 @@ public class box extends JFrame implements ActionListener {
         menu_Edit.add(item_copy);
         menu_Edit.add(item_stick);
         menu_Edit.add(item_selectAll);
-        //Edit 菜单
+        //The Edit menu
 
         menu_Help = new JMenu("Help");
         menu_Help.setMnemonic('h');
         item_about = new JMenuItem("About");
         menu_Help.add(item_about);
-        //Help 菜单
+        //The Help menu
 
         menuBar.add(menu_File);
         menuBar.add(menu_Search);
@@ -128,9 +129,9 @@ public class box extends JFrame implements ActionListener {
         menuBar.add(menu_Help);
     }
     /**
-     * 初始化编辑区域
-     * 用scrollpane装textarea
-     * 同时对pane设置方向
+     * Initializes the edit area
+     * Install the TextArea with the ScrollPane
+     * Also set the orientation on the Pane
      */
     public void initEditArea() {
         edit_text_pane = new JTextPane();
@@ -146,21 +147,21 @@ public class box extends JFrame implements ActionListener {
         popupMenu_Paste.addActionListener(this);
         popupMenu_SelectAll.addActionListener(this);
 
-        //文本编辑区注册右键菜单事件
+        //The text editor area registers right-click menu events
         edit_text_pane.addMouseListener(new MouseAdapter()
         { public void mousePressed(MouseEvent e)
-        { if(e.isPopupTrigger())//返回此鼠标事件是否为该平台的弹出菜单触发事件
-        { popupMenu.show(e.getComponent(),e.getX(),e.getY());//在组件调用者的坐标空间中的位置 X、Y 显示弹出菜单
+        { if(e.isPopupTrigger())//Returns whether this mouse event triggered an event for the platform's pop-up menu
+        { popupMenu.show(e.getComponent(),e.getX(),e.getY());//The positions X and Y in the component caller's coordinate space display the pop-up menu
         }
-            edit_text_pane.requestFocus();//编辑区获取焦点
+            edit_text_pane.requestFocus();//The edit area gets focus
         }
             public void mouseReleased(MouseEvent e)
-            { if(e.isPopupTrigger())//返回此鼠标事件是否为该平台的弹出菜单触发事件
-            { popupMenu.show(e.getComponent(),e.getX(),e.getY());//在组件调用者的坐标空间中的位置 X、Y 显示弹出菜单
+            { if(e.isPopupTrigger())//Returns whether this mouse event triggered an event for the platform's pop-up menu
+            { popupMenu.show(e.getComponent(),e.getX(),e.getY());//The positions X and Y in the component caller's coordinate space display the pop-up menu
             }
-                edit_text_pane.requestFocus();//编辑区获取焦点
+                edit_text_pane.requestFocus();//The edit area gets focus
             }
-        });//文本编辑区注册右键菜单事件结束
+        });//Text edit area registration right-click menu event ends
 
         item_search.addActionListener(this);
         item_new.addActionListener(this);
@@ -183,6 +184,7 @@ public class box extends JFrame implements ActionListener {
         }else if (e.getSource() == item_exit) {
             this.dispose();
         }else if (e.getSource() == item_open) {
+            System.out.println(json);
             fileFunction.open(edit_text_pane, scroll_bar);
         }else if (e.getSource() == item_save) {
             fileFunction.save(edit_text_pane, scroll_bar);
@@ -219,27 +221,32 @@ public class box extends JFrame implements ActionListener {
     }
 
     public class SyntaxHighlighter implements DocumentListener {
-        private Set<String> keywords;
-        private Style keywordStyle;
+        private Set<String> keywords1;
+        private Set<String> keywords2;
+        private Set<String> keywords3;
+        private Style keywordStyle1;
         private Style KeywordStyle2;
         private Style KeywordStyle3;
-        private Style KeywordStyle4;
         private Style normalStyle;
 
         public SyntaxHighlighter(JTextPane editor) {
 
-            // 准备着色使用的样式
-            keywordStyle = ((StyledDocument) editor.getDocument()).addStyle("Keyword_Style", null);
+            // Prepare the style for coloring
+            keywordStyle1 = ((StyledDocument) editor.getDocument()).addStyle("Keyword_Style", null);
             KeywordStyle2 = ((StyledDocument) editor.getDocument()).addStyle("Keyword_Style", null);
-
+            KeywordStyle3 = ((StyledDocument) editor.getDocument()).addStyle("Keyword_Style", null);
             normalStyle = ((StyledDocument) editor.getDocument()).addStyle("Keyword_Style", null);
-            StyleConstants.setForeground(keywordStyle, Color.orange);
+            StyleConstants.setForeground(keywordStyle1, Color.orange);
             StyleConstants.setForeground(KeywordStyle2, Color.yellow);
+            StyleConstants.setForeground(KeywordStyle3, Color.red);
             StyleConstants.setForeground(normalStyle, Color.BLACK);
 
 
-            // 准备关键字
-            keywords = new HashSet<String>();
+            // Prepare keywords
+            keywords1 = new HashSet<String>();
+            keywords2 = new HashSet<String>();
+            keywords3 = new HashSet<String>();
+
             JSONArray ja = jobj.getJSONArray("keys");
 
             for (int i=0;i<ja.size();i++){
@@ -249,12 +256,22 @@ public class box extends JFrame implements ActionListener {
                     while(iterator.hasNext()){
                         Map.Entry entry = (Map.Entry) iterator.next();
                         String value = entry.getValue().toString();
-                        keywords.add(value);
+                        keywords1.add(value);
                     }
                 }else if (i == 1){
-
+                    Iterator iterator = jo.entrySet().iterator();
+                    while(iterator.hasNext()) {
+                        Map.Entry entry = (Map.Entry) iterator.next();
+                        String value = entry.getValue().toString();
+                        keywords2.add(value);
+                    }
                 }else if (i == 2){
-
+                    Iterator iterator = jo.entrySet().iterator();
+                    while(iterator.hasNext()) {
+                        Map.Entry entry = (Map.Entry) iterator.next();
+                        String value = entry.getValue().toString();
+                        keywords3.add(value);
+                    }
                 }
             }
 
@@ -262,10 +279,9 @@ public class box extends JFrame implements ActionListener {
 
         public void colouring(StyledDocument doc, int pos, int len) throws BadLocationException {
 
-            // 取得插入或者删除后影响到的单词.
-            // 例如"public"在b后插入一个空格, 就变成了:"pub lic", 这时就有两个单词要处理:"pub"和"lic"
-            // 这时要取得的范围是pub中p前面的位置和lic中c后面的位置
-
+            // Gets the word affected after insert or delete.
+            // For example "public" insert a space after b and it becomes "public" and there are two words to deal with :"pub" and "lic"
+            // The range to get is the position before P in pub and after c in lic
             int start = indexOfWordStart(doc, pos);
             int end = indexOfWordEnd(doc, pos + len);
 
@@ -273,8 +289,8 @@ public class box extends JFrame implements ActionListener {
             while (start < end) {
                 ch = getCharAt(doc, start);
                 if (Character.isLetter(ch) || ch == '_') {
-                    // 如果是以字母或者下划线开头, 说明是单词
-                    // pos为处理后的最后一个下标
+                    // If it begins with a letter or an underscore, it's a word
+                    // Pos is the last subscript after processing
                     start = colouringWord(doc, start);
                 } else {
                     SwingUtilities.invokeLater(new ColouringTask(doc, start, 1, normalStyle));
@@ -285,84 +301,60 @@ public class box extends JFrame implements ActionListener {
         }
 
         /**
-         * 对单词进行着色, 并返回单词结束的下标.
-         * @param doc
-         * @param pos
-         * @return
-         * @throws BadLocationException
+         * Color the word and return the index at the end of the word.
          */
         public int colouringWord(StyledDocument doc, int pos) throws BadLocationException {
 
             int wordEnd = indexOfWordEnd(doc, pos);
             String word = doc.getText(pos, wordEnd - pos);
 
-            if (keywords.contains(word)) {
+            if (keywords1.contains(word)) {
 
-                // 如果是关键字, 就进行关键字的着色, 否则使用普通的着色.
-                // 这里有一点要注意, 在insertUpdate和removeUpdate的方法调用的过程中, 不能修改doc的属性.
-                // 但我们又要达到能够修改doc的属性, 所以把此任务放到这个方法的外面去执行.
-                // 实现这一目的, 可以使用新线程, 但放到swing的事件队列里去处理更轻便一点.
+            // If it is a keyword, color the keyword; otherwise, use normal coloring.
+            // One thing to note here is that during the method calls of insertUpdate and removeUpdate, the doc properties cannot be modified.
+            // But we want to be able to modify the doc properties, so we want to do this outside of this method.
+            // For this purpose, you can use new threads, but it is easier to put them into swing event queues.
+            SwingUtilities.invokeLater(new ColouringTask(doc, pos, wordEnd - pos, keywordStyle1));
+            }else if(keywords2.contains(word)){
+                SwingUtilities.invokeLater(new ColouringTask(doc, pos, wordEnd - pos, KeywordStyle2));
 
-                SwingUtilities.invokeLater(new ColouringTask(doc, pos, wordEnd - pos, keywordStyle));
-            } else {
+            }else if(keywords3.contains(word)){
+                SwingUtilities.invokeLater(new ColouringTask(doc, pos, wordEnd - pos, KeywordStyle3));
+            }
+            else {
                 SwingUtilities.invokeLater(new ColouringTask(doc, pos, wordEnd - pos, normalStyle));
             }
             return wordEnd;
         }
 
         /**
-         * 取得在文档中下标在pos处的字符.
-         * 如果pos为doc.getLength(), 返回的是一个文档的结束符, 不会抛出异常. 如果pos<0, 则会抛出异常.
-         * 所以pos的有效值是[0, doc.getLength()]
-         * @param doc
-         * @param pos
-         * @return
-         * @throws BadLocationException
-
+         * Gets the character subscripted at POS in the document..
+         * If the pos is doc.getLength(), it returns the end of a document and does not throw an exception.If pos&lt;0, an exception is thrown.
+         * So the valid value for pos is [0, doc.getLength()]
          */
         public char getCharAt(Document doc, int pos) throws BadLocationException {
 
             return doc.getText(pos, 1).charAt(0);
         }
 
-        /**
-         * 取得下标为pos时, 它所在的单词开始的下标. Â±wor^dÂ± (^表示pos, Â±表示开始或结束的下标)
-         * @param doc
-         * @param pos
-         * @return
-         * @throws BadLocationException
-         */
 
         public int indexOfWordStart(Document doc, int pos) throws BadLocationException {
 
-            // 从pos开始向前找到第一个非单词字符.
+            // Find the first non-word character forward from the POS..
             for (; pos > 0 && isWordCharacter(doc, pos - 1); --pos);
             return pos;
         }
 
-        /**
-         * 取得下标为pos时, 它所在的单词结束的下标. Â±wor^dÂ± (^表示pos, Â±表示开始或结束的下标)
-         * @param doc
-         * @param pos
-         * @return
-         * @throws BadLocationException
-         */
-
         public int indexOfWordEnd(Document doc, int pos) throws BadLocationException {
 
-            // 从pos开始向前找到第一个非单词字符.
+            // Find the first non-word character forward from the POS..
 
             for (; isWordCharacter(doc, pos); ++pos);
 
             return pos;
         }
         /**
-         * 如果一个字符是字母, 数字, 下划线, 则返回true.
-         *
-         * @param doc
-         * @param pos
-         * @return
-         * @throws BadLocationException
+         * Returns true if a character is alphabetic, numeric, or underlined.
          */
 
         public boolean isWordCharacter(Document doc, int pos) throws BadLocationException {
@@ -390,7 +382,7 @@ public class box extends JFrame implements ActionListener {
         @Override
         public void removeUpdate(DocumentEvent e) {
             try {
-                // 因为删除后光标紧接着影响的单词两边, 所以长度就不需要了
+                // Length is not needed because the cursor is immediately after the word is deleted
                 colouring((StyledDocument) e.getDocument(), e.getOffset(), 0);
             } catch (BadLocationException e1) {
                 e1.printStackTrace();
@@ -398,8 +390,7 @@ public class box extends JFrame implements ActionListener {
         }
 
         /**
-         * 完成着色任务
-         * @author Biao
+         * Complete the coloring task
          */
         private class ColouringTask implements Runnable {
             private StyledDocument doc;
@@ -417,7 +408,7 @@ public class box extends JFrame implements ActionListener {
 
             public void run() {
                 try {
-                    // 这里就是对字符进行着色
+                    // In this case, I'm coloring the characters
                     doc.setCharacterAttributes(pos, len, style, true);
                 } catch (Exception e) {}
             }
