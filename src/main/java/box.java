1380b6df46de9f3f08e6cf066cd52a6a4c8763f7
diodes12581 <2265984071@qq.com>
2020-10-09 11:57:02 +0800

    ChuqiWu

--- src/main/java/box.java
+++ src/main/java/box.java
@@ -1,3 +1,6 @@
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
 import com.lowagie.text.DocumentException;
 
 import javax.swing.*;
@@ -7,20 +10,31 @@ import javax.swing.text.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.io.IOException;
 import java.text.DateFormat;
 import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Map;
 import java.util.Set;
 
 public class box extends JFrame implements ActionListener {
 
+    String path = ReadJson.class.getClassLoader().getResource("package.json").getPath();
+    String json = ReadJson.readJsonFile(path);
+    JSONObject jobj = JSON.parseObject(json);
+
     private JMenuBar menuBar;
+    //右键弹出菜单项
+    JPopupMenu popupMenu;
+    JMenuItem popupMenu_Cut,popupMenu_Copy,popupMenu_Paste,popupMenu_SelectAll;
     //菜单栏
     private JMenu menu_File,menu_Search,menu_Edit,menu_Help;
     //菜单栏内的菜单
     private JMenuItem item_new,item_open,item_save,item_exit,item_print,item_export, item_search;
     //对于file菜单的子项
-    private JMenuItem item_undo,item_cut,item_copy,item_stick;
+    private JMenuItem item_selectAll,item_cut,item_copy,item_stick;
     //对于edit菜单的子项
     private JMenuItem item_about;
     //对于help菜单的子项
@@ -34,10 +48,10 @@ public class box extends JFrame implements ActionListener {
         initEditArea();
         initListener();
         this.setJMenuBar(menuBar);
-        this.setSize(800,600);
+        this.setSize((int)jobj.get("width"),(int)jobj.get("height"));
         DateFormat df = DateFormat.getDateTimeInstance();
         String current = df.format(System.currentTimeMillis());
-        this.setTitle("Editor             " + current);
+        this.setTitle(jobj.get("title") + current);
         this.add(scroll_bar);
         this.setVisible(true);
         this.setLocationRelativeTo(null);
@@ -51,6 +65,21 @@ public class box extends JFrame implements ActionListener {
      * item_word_format.setAccelerator(KeyStroke.getKeyStroke('F',java.awt.Event.CTRL_MASK,false)); 对item
      */
     public void initMenuBar() {
+
+        //创建右键弹出菜单
+        popupMenu=new JPopupMenu();
+        popupMenu_Cut=new JMenuItem("Cut(T)");
+        popupMenu_Copy=new JMenuItem("Copy(C)");
+        popupMenu_Paste=new JMenuItem("Paste(P)");
+        popupMenu_SelectAll=new JMenuItem("SelectAll(A)");
+
+
+        //向右键菜单添加菜单项和分隔符
+        popupMenu.add(popupMenu_Cut);
+        popupMenu.add(popupMenu_Copy);
+        popupMenu.add(popupMenu_Paste);
+        popupMenu.add(popupMenu_SelectAll);
+
         menuBar = new JMenuBar();
         menu_File = new JMenu("File");
         menu_File.setMnemonic('f');//f+alt打开
@@ -77,14 +106,14 @@ public class box extends JFrame implements ActionListener {
 
         menu_Edit = new JMenu("Edit");
         menu_Edit.setMnemonic('e');
-        item_undo = new JMenuItem("Undo");
         item_cut = new JMenuItem("Cut");
         item_copy = new JMenuItem("Copy");
         item_stick = new JMenuItem("Paste");
-        menu_Edit.add(item_undo);
+        item_selectAll = new JMenuItem("SelectAll");
         menu_Edit.add(item_cut);
         menu_Edit.add(item_copy);
         menu_Edit.add(item_stick);
+        menu_Edit.add(item_selectAll);
         //Edit 菜单
 
         menu_Help = new JMenu("Help");
@@ -112,6 +141,27 @@ public class box extends JFrame implements ActionListener {
 
 
     public void initListener() {
+        popupMenu_Cut.addActionListener(this);
+        popupMenu_Copy.addActionListener(this);
+        popupMenu_Paste.addActionListener(this);
+        popupMenu_SelectAll.addActionListener(this);
+
+        //文本编辑区注册右键菜单事件
+        edit_text_pane.addMouseListener(new MouseAdapter()
+        { public void mousePressed(MouseEvent e)
+        { if(e.isPopupTrigger())//返回此鼠标事件是否为该平台的弹出菜单触发事件
+        { popupMenu.show(e.getComponent(),e.getX(),e.getY());//在组件调用者的坐标空间中的位置 X、Y 显示弹出菜单
+        }
+            edit_text_pane.requestFocus();//编辑区获取焦点
+        }
+            public void mouseReleased(MouseEvent e)
+            { if(e.isPopupTrigger())//返回此鼠标事件是否为该平台的弹出菜单触发事件
+            { popupMenu.show(e.getComponent(),e.getX(),e.getY());//在组件调用者的坐标空间中的位置 X、Y 显示弹出菜单
+            }
+                edit_text_pane.requestFocus();//编辑区获取焦点
+            }
+        });//文本编辑区注册右键菜单事件结束
+
         item_search.addActionListener(this);
         item_new.addActionListener(this);
         item_open.addActionListener(this);
@@ -119,10 +169,10 @@ public class box extends JFrame implements ActionListener {
         item_exit.addActionListener(this);
         item_export.addActionListener(this);
         item_print.addActionListener(this);
-        item_undo.addActionListener(this);
         item_cut.addActionListener(this);
         item_copy.addActionListener(this);
         item_stick.addActionListener(this);
+        item_selectAll.addActionListener(this);
         item_about.addActionListener(this);
     }
 
@@ -145,145 +195,69 @@ public class box extends JFrame implements ActionListener {
                 ex.printStackTrace();
             }
         } else if (e.getSource() == item_search) {
-            search();
-        }else if (e.getSource() == item_undo) {
-            editFunction.undo(edit_text_pane);
+            search_function.search(this,edit_text_pane);
         }else if (e.getSource() == item_cut) {
             editFunction.cut(edit_text_pane);
         }else if (e.getSource() == item_copy) {
             editFunction.copy(edit_text_pane);
         }else if (e.getSource() == item_stick) {
             editFunction.paste(edit_text_pane);
-        }else if (e.getSource() == item_about) {
-            new helpFunction.about_Window();
+        }else if (e.getSource() == item_selectAll){
+            editFunction.selectAll(edit_text_pane);
+        }
+        else if (e.getSource() == item_about) {
+            new helpFunction.about_Window(jobj);
+        }else if (e.getSource() == popupMenu_Cut){
+            editFunction.cut(edit_text_pane);
+        }else if (e.getSource() == popupMenu_Copy){
+            editFunction.cut(edit_text_pane);
+        }else if (e.getSource() == popupMenu_Paste){
+            editFunction.paste(edit_text_pane);
+        }else if (e.getSource() == popupMenu_SelectAll){
+            editFunction.selectAll(edit_text_pane);
         }
     }
-
-
-    public void search()
-    {   final JDialog findDialog=new JDialog(this,"查找",false);//false时允许其他窗口同时处于激活状态(即无模式)
-        Container con=findDialog.getContentPane();//返回此对话框的contentPane对象
-        con.setLayout(new FlowLayout(FlowLayout.LEFT));
-        JLabel findContentLabel=new JLabel("查找内容(N)：");
-        final JTextField findText=new JTextField(15);
-        JButton findNextButton=new JButton("查找下一个(F)：");
-        final JCheckBox matchCheckBox=new JCheckBox("区分大小写(C)");
-        ButtonGroup bGroup=new ButtonGroup();
-        final JRadioButton upButton=new JRadioButton("向上(U)");
-        final JRadioButton downButton=new JRadioButton("向下(U)");
-        downButton.setSelected(true);
-        bGroup.add(upButton);
-        bGroup.add(downButton);
-        /*ButtonGroup此类用于为一组按钮创建一个多斥（multiple-exclusion）作用域。
-        使用相同的 ButtonGroup 对象创建一组按钮意味着“开启”其中一个按钮时，将关闭组中的其他所有按钮。*/
-        /*JRadioButton此类实现一个单选按钮，此按钮项可被选择或取消选择，并可为用户显示其状态。
-        与 ButtonGroup 对象配合使用可创建一组按钮，一次只能选择其中的一个按钮。
-        （创建一个 ButtonGroup 对象并用其 add 方法将 JRadioButton 对象包含在此组中。）*/
-        JButton cancel=new JButton("取消");
-        //取消按钮事件处理
-        cancel.addActionListener(new ActionListener()
-        {   public void actionPerformed(ActionEvent e)
-        {   findDialog.dispose();
-        }
-        });
-        //"查找下一个"按钮监听
-        findNextButton.addActionListener(new ActionListener()
-        {   public void actionPerformed(ActionEvent e)
-        {   //"区分大小写(C)"的JCheckBox是否被选中
-            int k=0,m=0;
-            final String str1,str2,str3,str4,strA,strB;
-            str1= edit_text_pane.getText();
-            str2=findText.getText();
-            str3=str1.toUpperCase();
-            str4=str2.toUpperCase();
-            if(matchCheckBox.isSelected())//区分大小写
-            {   strA=str1;
-                strB=str2;
-            }
-            else//不区分大小写,此时把所选内容全部化成大写(或小写)，以便于查找
-            {   strA=str3;
-                strB=str4;
-            }
-            if(upButton.isSelected())
-            {   //k=strA.lastIndexOf(strB,editArea.getCaretPosition()-1);
-                if(edit_text_pane.getSelectedText()==null)
-                    k=strA.lastIndexOf(strB, edit_text_pane.getCaretPosition()-1);
-                else
-                    k=strA.lastIndexOf(strB, edit_text_pane.getCaretPosition()-findText.getText().length()-1);
-                if(k>-1)
-                {   //String strData=strA.subString(k,strB.getText().length()+1);
-                    edit_text_pane.setCaretPosition(k);
-                    edit_text_pane.select(k,k+strB.length());
-                }
-                else
-                {   JOptionPane.showMessageDialog(null,"找不到您查找的内容！","查找",JOptionPane.INFORMATION_MESSAGE);
-                }
-            }
-            else if(downButton.isSelected())
-            {   if(edit_text_pane.getSelectedText()==null)
-                k=strA.indexOf(strB, edit_text_pane.getCaretPosition()+1);
-            else
-                k=strA.indexOf(strB, edit_text_pane.getCaretPosition()-findText.getText().length()+1);
-                if(k>-1)
-                {   //String strData=strA.subString(k,strB.getText().length()+1);
-                    edit_text_pane.setCaretPosition(k);
-                    edit_text_pane.select(k,k+strB.length());
-                }
-                else
-                {   JOptionPane.showMessageDialog(null,"找不到您查找的内容！","查找",JOptionPane.INFORMATION_MESSAGE);
-                }
-            }
-        }
-        });//"查找下一个"按钮监听结束
-        //创建"查找"对话框的界面
-        JPanel panel1=new JPanel();
-        JPanel panel2=new JPanel();
-        JPanel panel3=new JPanel();
-        JPanel directionPanel=new JPanel();
-        directionPanel.setBorder(BorderFactory.createTitledBorder("方向"));
-        //设置directionPanel组件的边框;
-        //BorderFactory.createTitledBorder(String title)创建一个新标题边框，使用默认边框（浮雕化）、默认文本位置（位于顶线上）、默认调整 (leading) 以及由当前外观确定的默认字体和文本颜色，并指定了标题文本。
-        directionPanel.add(upButton);
-        directionPanel.add(downButton);
-        panel1.setLayout(new GridLayout(2,1));
-        panel1.add(findNextButton);
-        panel1.add(cancel);
-        panel2.add(findContentLabel);
-        panel2.add(findText);
-        panel2.add(panel1);
-        panel3.add(matchCheckBox);
-        panel3.add(directionPanel);
-        con.add(panel2);
-        con.add(panel3);
-        findDialog.setSize(410,180);
-        findDialog.setResizable(false);//不可调整大小
-        findDialog.setLocation(230,280);
-        findDialog.setVisible(true);
-    }//查找方法结束
 
     public class SyntaxHighlighter implements DocumentListener {
         private Set<String> keywords;
         private Style keywordStyle;
+        private Style KeywordStyle2;
+        private Style KeywordStyle3;
+        private Style KeywordStyle4;
         private Style normalStyle;
 
         public SyntaxHighlighter(JTextPane editor) {
 
             // 准备着色使用的样式
             keywordStyle = ((StyledDocument) editor.getDocument()).addStyle("Keyword_Style", null);
+            KeywordStyle2 = ((StyledDocument) editor.getDocument()).addStyle("Keyword_Style", null);
+
             normalStyle = ((StyledDocument) editor.getDocument()).addStyle("Keyword_Style", null);
             StyleConstants.setForeground(keywordStyle, Color.orange);
+            StyleConstants.setForeground(KeywordStyle2, Color.yellow);
             StyleConstants.setForeground(normalStyle, Color.BLACK);
 
 
             // 准备关键字
             keywords = new HashSet<String>();
-            keywords.add("public");
-            keywords.add("class");
-            keywords.add("protected");
-            keywords.add("private");
-            keywords.add("_int_");
-            keywords.add("float");
-            keywords.add("double");
+            JSONArray ja = jobj.getJSONArray("keys");
+
+            for (int i=0;i<ja.size();i++){
+                JSONObject jo = ja.getJSONObject(i);
+                if(i == 0){
+                    Iterator iterator = jo.entrySet().iterator();
+                    while(iterator.hasNext()){
+                        Map.Entry entry = (Map.Entry) iterator.next();
+                        String value = entry.getValue().toString();
+                        keywords.add(value);
+                    }
+                }else if (i == 1){
+
+                }else if (i == 2){
+
+                }
+            }
+
         }
 
         public void colouring(StyledDocument doc, int pos, int len) throws BadLocationException {
